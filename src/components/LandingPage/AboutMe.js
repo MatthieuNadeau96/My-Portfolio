47d3d8d0f75cbe50cc2af5963107bae8444acb08
Matthieu <matthieunadeau96@gmail.com>
2018-08-14 12:33:08 -0400

    changed landing aboutme

--- src/components/LandingPage/AboutMe.js
+++ src/components/LandingPage/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutMe = props => (
   <div className="about-container">
-    <p>I am a self-taught web developer from a small town in Massachussetts. I love creating efficient and beautifily simple thingys.</p>
+    <p>I am a self-taught web developer from a small town in Massachussetts. I love creating beautiful and efficiently simple things.</p>
   </div>
 );
 
