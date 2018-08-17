2ae10f7f8f1435bbabe2fd3f71bb4e67cc597420
Matthieu <matthieunadeau96@gmail.com>
2018-08-17 10:42:04 -0400

    added a down arrow on the landing page to navigate to the project section

--- src/components/LandingPage/_LandingPage.js
+++ src/components/LandingPage/_LandingPage.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
+
 class LandingPage extends Component {
+
+  goDown = () => {
+    // window.scrollTo(0, 940);
+    window.scroll({ top: 940, left: 0, behavior: 'smooth'});
+  }
+
   render() {
     return (
       <div className="home-section">
         <div className="landing-text">
           <p className="about-me">Hello, my name is <span className="landing-title">Matthieu Nadeau</span>.
           <br/>I am a self-taught developer from a small town in Massachusetts. I love creating beautiful and efficiently simple things.</p>
+        </div>
+        <div className="down-arrow" onClick={this.goDown}>
+          <i className="fas fa-angle-down"/>
         </div>
       </div>
     );
