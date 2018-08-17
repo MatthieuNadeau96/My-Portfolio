878bed1fff4d3fa7e641fefd15e5be54d320ec08
Matthieu <matthieunadeau96@gmail.com>
2018-08-17 11:45:27 -0400

    added responsiveness to the down-arrow

--- src/components/LandingPage/_LandingPage.js
+++ src/components/LandingPage/_LandingPage.js
@@ -13,9 +13,9 @@ class LandingPage extends Component {
         <div className="landing-text">
           <p className="about-me">Hello, my name is <span className="landing-title">Matthieu Nadeau</span>.
           <br/>I am a self-taught developer from a small town in Massachusetts. I love creating beautiful and efficiently simple things.</p>
-        </div>
-        <div className="down-arrow" onClick={this.goDown}>
-          <i className="fas fa-angle-down"/>
+          <div className="down-arrow" onClick={this.goDown}>
+            <i className="fas fa-angle-down"/>
+          </div>
         </div>
       </div>
     );
