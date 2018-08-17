ed3da026756193521a547cc71a888f349c33b7bb
Matthieu <matthieunadeau96@gmail.com>
2018-08-17 10:54:33 -0400

    Updates

--- src/components/ContactPage/Signature.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Signature = props => (
-  <div>
-    <p className="signature">Developed and Designed by Matthieu Nadeau <i className="far fa-copyright"></i> 2018</p>
-  </div>
-);
-
-export default Signature;
